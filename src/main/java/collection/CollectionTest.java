2bd959b2488690d55697ddcaed8d93172e04cd54
ntan <nicolastan27@gmail.com>
2024-04-09 14:50:27 +0200

    feat: add collectiontest functions

--- src/main/java/collection/CollectionTest.java
+++ src/main/java/collection/CollectionTest.java
@@ -1,6 +1,7 @@
 package collection;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * You should complete the function in this class
@@ -8,20 +9,28 @@ import java.util.List;
  * Feel free to define any method and / or class you want
  */
 class CollectionTest {
-
-
   /**
    * operation : x -> ((x * 2) + 3) ^ 5
    */
   public static List<Double> compute1(List<Integer> input) {
-    return null;
+    return input.stream()
+        .map(i -> Math.pow(((i * 2) + 3), 5))
+        .collect(Collectors.toList());
   }
 
   /**
    * operation : abc -> AbcAbc
    */
   public static List<String> compute2(List<String> input) {
-    return null;
+    return input.stream()
+        .map(i -> capitalize(i) + capitalize(i))
+        .collect(Collectors.toList());
   }
 
+  private static String capitalize(String str) {
+    if (str == null || str.isEmpty()) {
+      return "";
+    }
+    return str.substring(0, 1).toUpperCase().concat(str.substring(1));
+  }
 }
