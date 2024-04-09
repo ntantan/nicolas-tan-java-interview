8903818abf30b1fd0c4fe2d53a65a9a13df6d72d
ntan <nicolastan27@gmail.com>
2024-04-09 14:50:20 +0200

    feat: add basictest functions

--- src/main/java/basic/BasicTest.java
+++ src/main/java/basic/BasicTest.java
@@ -12,6 +12,21 @@ public class BasicTest {
    * alse return None in case of errors
    */
   public static Option<Integer> power(Integer i, Integer n) {
-    return Option.none();
-  }
+    if (i == null || n == null || n < 0) {
+      return Option.none();
+    }
+    
+    if (n == 0) {
+      return Option.some(1);
+    }
+  
+    Long result = i.longValue();
+    for (int j = 1; j < n; j++) {
+      result *= i;
+      if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
+        return Option.none();
+      }
+    }
+    return Option.some(result.intValue());
+ }
 }
